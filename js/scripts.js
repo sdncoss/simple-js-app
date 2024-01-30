92fcfecee8c688cc564e432a154f20d3c4d63855
sdncoss <157067345+sdncoss@users.noreply.github.com>
2024-01-30 12:43:09 -0800

    Added first array

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+let pokemonList = [
+    {name: 'Bulbasaur', height:  0.7, type: ['Grass','Poison']},
+    {name: 'Charmander', height: 0.6, type: ['Fire', 'Steel']},
+    {name: 'Squirtle', height: 0.5, type: ['Water', 'Steel']},
+    {name: 'Pikachu', height: 0.4, type: ['Electric', 'Psychic']},
+    {name: 'Clefairy', height: 0.6, type: ['Psychic', 'Fairy']},
+];
