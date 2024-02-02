4f42b8e8e610955e99b344c51c107173a08926d7
sdncoss <157067345+sdncoss@users.noreply.github.com>
2024-02-02 12:30:37 -0800

    edited type to just one description

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 // list of pokemon for the app
 let pokemonList = [
-    {name: 'Bulbasaur', height:  0.7, type: ['Grass','Poison']},
-    {name: 'Charmander', height: 0.6, type: ['Fire', 'Steel']},
-    {name: 'Squirtle', height: 0.5, type: ['Water', 'Steel']},
-    {name: 'Pikachu', height: 0.4, type: ['Electric', 'Psychic']},
-    {name: 'Clefairy', height: 0.6, type: ['Psychic', 'Fairy']},
+    {name: 'Bulbasaur', height:  0.7, type: ['Grass']},
+    {name: 'Charmander', height: 0.6, type: ['Fire']},
+    {name: 'Squirtle', height: 0.5, type: ['Water']},
+    {name: 'Pikachu', height: 0.4, type: ['Electric']},
+    {name: 'Clefairy', height: 0.6, type: ['Psychic']},
 ];
 
-// loop to write pokemon name and height
+// loop to write pokemon name and height within scale of heights
 for (let i = 0; i < pokemonList.length; i++) {
     if (pokemonList[i].height > 0.5 && pokemonList[i].height < 1){
         document.write(pokemonList[i].name + ' is average at ' + pokemonList[i].height + 'cm tall.<br> ');
