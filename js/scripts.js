3dd21bcf9c96d051f18ac18dcecfc9499d6a267f
sdncoss <157067345+sdncoss@users.noreply.github.com>
2024-02-08 10:38:13 -0800

    added comments and event listener to button

--- js/scripts.js
+++ js/scripts.js
@@ -7,21 +7,37 @@ let pokemonRepository = (function () {
         { name: 'Pikachu', height: 0.4, types: ['Electric', 'Psychic'] },
         { name: 'Clefairy', height: 0.6, types: ['Psychic', 'Fairy'] }
     ];
+    // function to add new pokemon to the list
     function add(pokemon) {
         pokemonList.push(pokemon);
     }
+    // function to get all the pokemon in the array
     function getAll() {
         return pokemonList;
     }
     //function using DOM to use the array as a list of buttons
     function addListItem(pokemon) {
+        // create variable for the ul of pokemon-list
         let pokemonNames = document.querySelector('.pokemon-list');
+        // create the list
         let listItem = document.createElement('li');
+        // create buttons for each list item
         let button = document.createElement('button');
+        // uses the array to put pokemon names on each button
         button.innerText = pokemon.name;
+        // calls the CSS styling for buttons
         button.classList.add('pokemon-button');
+        // puts buttons in the list
         listItem.appendChild(button);
+        // puts li in ul 
         pokemonNames.appendChild(listItem);
+        // create click button event
+        button.addEventListener('click', function (event){
+            showDetails(pokemon);
+        })
+    }
+    function showDetails(pokemon){
+        console.log(pokemon);
     }
     return {
         add: add,
